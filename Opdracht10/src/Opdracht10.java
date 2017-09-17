1a0cd1dfe0d49b45dd9cd97ef133eadaf5ce43d0
TwoAcesVI <32024721+TwoAcesVI@users.noreply.github.com>
2017-09-17 17:21:34 +0200

    Add files via upload

---
+++ Opdracht10/src/Opdracht10.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Opdracht10 {
+
+    public static void main(String[] args){
+        Scanner Player1 = new Scanner(System.in);
+        Scanner Player2 = new Scanner(System.in);
+        String A = "1";
+        String B = "2";
+        String C = "3";
+        String D = "4";
+        String E = "O";
+        String F = "X";
+        String G = "7";
+        String H = "8";
+        String I = "9";
+
+        System.out.println(A + " | " + B + " | " + C);
+        System.out.println("--------");
+        System.out.println(D + " | " + E + " | " + F);
+        System.out.println("---------");
+        System.out.println(G + " | " + H + " | " + I);
+    }
+}
